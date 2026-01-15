b1d385341f85fecefdf37626eaa0ee361c96adc3
takashiraki <stgtbike4649@gmail.com>
2026-01-15 14:26:00 +0900

    wip

--- app/http-server.c
+++ app/http-server.c
@@ -1,5 +1,7 @@
 #include <arpa/inet.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
@@ -22,4 +24,37 @@ int main(void)
     addr.sin_family = AF_INET;         // IPv4を使うことを示す
     addr.sin_port = htons(8080);       // htonsはOSのエンディアンに合わせる関数
     addr.sin_addr.s_addr = INADDR_ANY; // ここですべてのIPを受け付けるらしい
+
+    // バインドするよ
+    // ここで8080ポート向けの全てのIPからのリクエストを受け付けるよう紐づける
+
+    // この1行で「server_fdというソケットを、
+    // ポート8080・すべてのIPアドレス（INADDR_ANY）で待ち受けるように設定する」
+    // ということをOSに伝えています。
+    bind(server_fd, (struct sockaddr *)&addr, sizeof(addr));
+
+    // ここでリッスンするよ（接続町状態にする）
+    listen(server_fd, 5);
+
+    printf("listening on port 8080...\n");
+
+    // ここでリクエストを受け入れる
+    client_fd = accept(server_fd, NULL, NULL);
+
+    if (client_fd < 0){
+        perror("accept failed");
+        return 1;
+    }
+
+    // リクエストを読むよ
+    char buffer[1024];
+    int n = read(client_fd, buffer, sizeof(buffer) - 1);
+
+    if (n <= 0)
+    {
+        perror("read failed");
+        close(client_fd);
+        close(server_fd);
+        return 1;
+    }
 }
