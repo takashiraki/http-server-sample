77bb5e1682d0855b06456a51ac6cc4af0c0e300e
takashiraki <stgtbike4649@gmail.com>
2026-01-19 07:39:45 +0900

    wip

--- app/http-server.c
+++ app/http-server.c
@@ -31,53 +31,60 @@ int main(void)
     // この1行で「server_fdというソケットを、
     // ポート8080・すべてのIPアドレス（INADDR_ANY）で待ち受けるように設定する」
     // ということをOSに伝えています。
-    bind(server_fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind failed");
+        return 1;
+    }
 
     // ここでリッスンするよ（接続待ち状態にする）
-    listen(server_fd, 5);
+    if (listen(server_fd, 5) < 0)
+    {
+        perror("listen failed");
+        return 1;
+    }
 
     printf("listening on port 8080...\n");
 
-    // ここでリクエストを受け入れる
-    client_fd = accept(server_fd, NULL, NULL);
-
-    if (client_fd < 0)
+    while (1)
     {
-        perror("accept failed");
-        return 1;
-    }
+        // ここでリクエストを受け入れる
+        client_fd = accept(server_fd, NULL, NULL);
 
-    // リクエストを読むよ
-    char buffer[1024];
-    int n = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (client_fd < 0)
+        {
+            perror("accept failed");
+            return 1;
+        }
 
-    if (n <= 0)
-    {
-        perror("read failed");
+        // リクエストを読むよ
+        char buffer[1024];
+        int n = read(client_fd, buffer, sizeof(buffer) - 1);
+
+        if (n > 0)
+        {
+            buffer[n] = '\0'; // null終端
+
+            printf("REQUEST:\n%s\n", buffer);
+
+            // レスポンスを書く
+            const char *body = "Hello world";
+            char response[256];
+
+            int len = snprintf(response, sizeof(response),
+                               "HTTP/1.1 200 OK\r\n"
+                               "Content-Length: %zu\r\n"
+                               "Connection: close\r\n"
+                               "\r\n"
+                               "%s",
+                               strlen(body), body);
+
+            write(client_fd, response, len);
+        }
+
+        // ソケットを閉じる
         close(client_fd);
-        close(server_fd);
-        return 1;
     }
 
-    buffer[n] = '\0'; // null終端
-
-    printf("REQUEST:\n%s\n", buffer);
-
-    // レスポンスを書く
-    const char *body = "Hello world";
-    char response[256];
-
-    int len = snprintf(response, sizeof(response),
-                       "HTTP/1.1 200 OK\r\n"
-                       "Content-Length: %zu\r\n"
-                       "Connection: close\r\n"
-                       "\r\n"
-                       "%s",
-                       strlen(body), body);
-
-    write(client_fd, response, len);
-
-    // ソケットを閉じる
-    close(client_fd);
     close(server_fd);
 }
